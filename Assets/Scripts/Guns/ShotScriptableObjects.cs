a22accecc70942d17d98fe04a57341f88ae671c9
Inocybe <140019717+Inocybe@users.noreply.github.com>
2023-12-11 13:24:42 -0800

    start corutine not working

--- Assets/Scripts/Guns/ShotScriptableObjects.cs
+++ Assets/Scripts/Guns/ShotScriptableObjects.cs
@@ -5,11 +5,40 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "BulletScriptableObject", menuName = "ScriptableObjects/bullet")]
 public class BulletScriptableObject : ScriptableObject
 {
-    [SerializeField] public Transform shootPoint;
+    [SerializeField] public GameObject bullet;
+    [HideInInspector] public Transform shootPoint;
     public float bulletSpeed;
     public float destroyTimer;
+    public float shootCooldown;
 
-    private GameObject _shootPoint;
+    private float _shootCooldown;
+    private float _originalDestroyTimer;
 
+    public void SetCooldown()
+    {
+        _shootCooldown = shootCooldown;
+        _originalDestroyTimer = destroyTimer;
+    }
 
+    private GameObject _shotBullet;
+    
+    public void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.J) && _shootCooldown <= 0)
+        {
+            _shotBullet = Instantiate(bullet, shootPoint.transform.position, Quaternion.identity); 
+            Rigidbody rb = _shotBullet.GetComponent<Rigidbody>();
+            rb.AddForce(bulletSpeed * Time.deltaTime, 0f, 0f, ForceMode.Impulse);
+            StartCoroutine(StartBulletTimer());
+            
+            _shootCooldown = shootCooldown;
+        }
+        _shootCooldown -= Time.deltaTime;
+    }
+
+    IEnumerator StartBulletTimer()
+    {
+        yield return new WaitForSeconds(shootCooldown);
+        Destroy(_shotBullet);
+    }
 }
--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -5,36 +5,12 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
-    [SerializeField] private GameObject bullet;
-    [SerializeField] private Transform shootPoint;
-
-    public float shootCooldown;
-
-    private float _originalShootCooldown;
-
-    private void Start()
-    {
-        _originalShootCooldown = shootCooldown;
-    }
+   
 
     // Update is called once per frame
     void Update()
     {
-        shootCooldown -= Time.deltaTime;
-
-        if (shootCooldown <= 0)
-        {
-            Shoot();
-        }
+        //shootObject.Shoot();
     }
-
-    private void Shoot()
-    {
-       if (Input.GetKeyDown(KeyCode.J))
-       {
-           GameObject instantiatedBullet = Instantiate(bullet, shootPoint.position, Quaternion.identity); 
-           //instantiatedBullet.transform.position = shootPoint.position;
-           shootCooldown = _originalShootCooldown;
-       }
-    }
+    
 }
